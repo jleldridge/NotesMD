7264b6d543ef64565e1378844ad7433eb930e104
Luke Eldridge <jleldridge27@gmail.com>
2020-05-10 17:38:32 -0700

    Add ability to add notes to the note list.

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Button, FlatList, StyleSheet, View } from "react-native";
 import { addNote } from "../redux/actions";
 import Note from "./Note";
 import { RootState, Note as TNote } from "../types";
 
-type Props = {
-  notes?: TNote[];
-};
+export default function NotesList() {
+  const notes = useSelector((state: RootState) => state.notes);
+  const dispatch = useDispatch();
+  const onAddNote = useCallback(() => {
+    dispatch(addNote(true, ""));
+  }, [dispatch]);
 
-export default function NotesList(props: Props) {
   return (
     <View>
       <FlatList
-        data={props.notes}
+        data={notes}
         renderItem={({ item }) => (
           <View style={styles.note}>
             <Note editing={item.editing} content={item.content} />
@@ -24,8 +26,6 @@ export default function NotesList(props: Props) {
     </View>
   );
 }
-
-function onAddNote() {}
 
 const styles = StyleSheet.create({
   note: {
